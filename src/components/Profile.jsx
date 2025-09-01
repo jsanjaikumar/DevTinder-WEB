f66bebf6d6adab9bb8735a9d466a7d9360d9463f
Sanjai Kumar <youremail@example.com>
2025-09-01 19:31:42 +0530

    user cards and Profile edit API setup

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import EditProfile from './EditProfile'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const user = useSelector((store)=> store.user)
+  console.log(user)
   return (
-    <div>Profile</div>
+   ( user &&
+    <>
+    <EditProfile user={user}/>
+    </>
+   )
   )
 }
 
--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -8,12 +8,14 @@ const { firstName, lastName, age, about, photoUrl, skills, gender } = user
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
       <figure>
-        <img src={photoUrl} alt="Shoes" />
+        <img src={photoUrl} alt="user Image" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
+        {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
-        {age && gender && <p>{age + " " + gender}</p>}
+        {skills && <p>{skills}</p>}
+
         <div className="card-actions justify-center my-4 ">
           <button className="btn btn-primary mx-4">Ignore</button>
           <button className="btn btn-secondary mx-4">Interested</button>
