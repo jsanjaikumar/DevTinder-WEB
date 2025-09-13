6eb17ebfcb264b9bfa5ed69cce00077736e7e90a
Sanjai Kumar <youremail@example.com>
2025-09-13 14:12:24 +0530

    policy setup for razorpay integration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -60,6 +60,9 @@ const Navbar =  () =>  {
                 <Link to="/connections">Connections</Link>
               </li>
               <li>
+                <Link to="/premium">Premium</Link>
+              </li>
+              <li>
                 <Link to="/requests">Requests</Link>
               </li>
               <li>
